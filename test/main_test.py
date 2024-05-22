416d82d787ba070acc0716a2fb21cfac899ff0b7
Tibor Casteleijn <tibbor.casteleijn@easytrip.eu>
2024-05-22 10:52:40 +0200

    add test

---
+++ test/main_test.py
@@ -0,0 +1,13 @@
+import unittest
+import subprocess
+
+class TestContainerResponse(unittest.TestCase):
+    def test_container_hello_response(self):
+        subprocess.run(['docker', 'compose', 'build'])
+        result = subprocess.run(['docker', 'run', 'tiborrr/docspell-addon-example', 'John'], capture_output=True, text=True)
+        
+        # Check if the output from the container is as expected
+        self.assertEqual(result.stdout.strip(), 'Hello John')
+
+if __name__ == '__main__':
+    unittest.main()
